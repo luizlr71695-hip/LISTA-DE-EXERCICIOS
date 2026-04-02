5fc3b78f88f34f67e3eca78f898103e57f17cd5b
Luiz Ricardo <luiz1456@hotmail.com>
2026-04-02 20:44:05 -0300

    first commit

---
+++ listadeExercicios01.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Digite o comprimento da caixa retangular: ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a largura da caixa retangular: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a altura da caixa retangular: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double volume = comprimento * largura * altura;
+
+Console.WriteLine($"O volume da caixa é: {volume:F2} cm³");
+
+Console.ReadLine();
+
+
