61a52d797951201d52ba9c64a797e2ec1ebfaacd
Luiz Ricardo <luiz1456@hotmail.com>
2026-04-06 22:56:32 -0300

    concluido exercicio 03.

---
+++ Exercicio03ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("Digite a temperatura que deseja ser convertida:");
+double temperatura = Convert.ToDouble(Console.ReadLine());
+
+
+double multiplicavel = 1.8;
+
+double formula = 32;
+
+double resultado = temperatura * multiplicavel + formula;
+
+Console.WriteLine($"O resultado da conversão é : {resultado:f2}");
+
+
+
+
+
