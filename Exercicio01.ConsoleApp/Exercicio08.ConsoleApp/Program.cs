b20c93f7389b17168e75886d600d0ca4681d692a
Luiz Ricardo <luiz1456@hotmail.com>
2026-04-08 23:37:38 -0300

    Exercicio 08 concluido.

---
+++ Exercicio01.ConsoleApp/Exercicio08.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Digite um numero: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("o seu numero é par. ");
+}
+
+else
+{
+    Console.WriteLine("O seu numero é impar. ");
+}
