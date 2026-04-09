331b83a77e3d12d31fe2f304da2b25b308fee109
Luiz Ricardo <luiz1456@hotmail.com>
2026-04-08 22:57:30 -0300

    Exercicio 06 concluido.

---
+++ Exercicio01.ConsoleApp/Exercicio06.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Digite o valor de A: ");
+decimal a = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B: ");
+decimal b = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de C: ");
+decimal c = Convert.ToDecimal(Console.ReadLine());
+
+if (a + b < c)
+{
+    Console.WriteLine("A soma do valor A e B é menor que C.");
+}
+
+else
+{
+    Console.WriteLine("A soma do valor de A e B não é menor que C.");
+}
