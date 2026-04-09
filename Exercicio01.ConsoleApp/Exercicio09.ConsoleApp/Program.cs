9d671b9473f825847794e5a031a31eb991948ebb
Luiz Ricardo <luiz1456@hotmail.com>
2026-04-08 23:59:18 -0300

    Exercicio 07 concluido.

---
+++ Exercicio01.ConsoleApp/Exercicio09.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿int a, fatorial = 1;
+
+Console.Write("Digite um valor para A: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(a + "! = ");
+for (int i = a; i >= 1; i--) ;
+{
+    Console.Write(i + " ");
+    fatorial *= i;
+}
+
+if (i > 1)
+{
+    Console.Write("x ");
+}
