9720f7d86b2567a48b368cf6cb79d5b35e8e8eb6
Luiz Ricardo <luiz1456@hotmail.com>
2026-04-09 00:33:19 -0300

    Conclusão Lista de Exericicios.

---
+++ Exercicio01.ConsoleApp/Exercicio10.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("fizzbuzz ");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write("fizz ");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write("buzz ");
+    }
+    else
+    {
+        Console.Write(Convert.ToString(i) + " ");
+    }
+}
